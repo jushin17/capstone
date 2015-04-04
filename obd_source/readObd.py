64bc445be47439f74fe065098bdbb51332bc9fa2
JIUNG SHIN <jushin17@gmail.com>
2015-04-04 17:32:33 +0900

    read obd simulation data that written by json, set the judgement data

---
+++ obd_source/readObd.py
@@ -0,0 +1,17 @@
+import json
+import analyis
+
+
+class ReadJson(DataScore)
+    def read(self):
+        with open('test.json') as f:
+            for line in f:
+                test = json.loads(line)
+        
+                if(test['name']=='vehicle_speed'):
+                    DataScore.dataScore[speed]=test['value']
+                elif(test['name']=='transmission_gear_position'):
+                    DataScore.dataScore[gear]=test['value']
+                elif(test['name']=='steering_wheel_angle'):
+                    DataScore.dataScore[steering]=test['value']
+            
