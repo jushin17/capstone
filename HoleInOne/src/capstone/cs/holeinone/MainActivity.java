0f2b107e8ffa3c3efc0c1eb01cc6cb9ee4b678bf
HyunSongKim <gusthd92@gmail.com>
2015-04-06 21:43:20 +0900

    select function page

--- HoleInOne/src/capstone/cs/holeinone/MainActivity.java
+++ HoleInOne/src/capstone/cs/holeinone/MainActivity.java
@@ -1,5 +1,5 @@
 package capstone.cs.holeinone;
-//dddddddddd
+
 import capstone.cs.holeinone.R;
 import android.app.Activity;
 import android.app.PendingIntent;
@@ -10,92 +10,23 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    Context mContext;
-    EditText smsNumber, smsTextContext;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mContext = this;
-        
-        smsNumber = (EditText) findViewById(R.id.smsNumber);
-        smsTextContext = (EditText) findViewById(R.id.smsText);
-    }
-    
-    public void sendSMS(View v){
-        String smsNum = smsNumber.getText().toString();
-        String smsText = smsTextContext.getText().toString();
-        
-        if (smsNum.length()>0 && smsText.length()>0){
-            sendSMS(smsNum, smsText);
-        }else{
-            Toast.makeText(this, "모두 입력해 주세요", Toast.LENGTH_SHORT).show();
-        }
-    }
-    //sms 전송
-    public void sendSMS(String smsNumber, String smsText){
-        PendingIntent sentIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_SENT_ACTION"), 0);
-        PendingIntent deliveredIntent = PendingIntent.getBroadcast(this, 0, new Intent("SMS_DELIVERED_ACTION"), 0);
-        
-        /**
-         * SMS가 발송될때 실행
-         * When the SMS massage has been sent
-         */
-        registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                switch(getResultCode()){
-                    case Activity.RESULT_OK:
-                        // 전송 성공
-                        Toast.makeText(mContext, "전송 완료", Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        // 전송 실패
-                        Toast.makeText(mContext, "전송 실패", Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        // 서비스 지역 아님
-                        Toast.makeText(mContext, "서비스 지역이 아닙니다", Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        // 무선 꺼짐
-                        Toast.makeText(mContext, "무선(Radio)가 꺼져있습니다", Toast.LENGTH_SHORT).show();
-                        break;
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        // PDU 실패
-                        Toast.makeText(mContext, "PDU Null", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter("SMS_SENT_ACTION"));
-        
-        /**
-         * SMS가 도착했을때 실행
-         * When the SMS massage has been delivered
-         */
-        registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                switch (getResultCode()){
-                    case Activity.RESULT_OK:
-                        // 도착 완료
-                        Toast.makeText(mContext, "SMS 도착 완료", Toast.LENGTH_SHORT).show();
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        // 도착 안됨
-                        Toast.makeText(mContext, "SMS 도착 실패", Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        }, new IntentFilter("SMS_DELIVERED_ACTION"));
-        
-        SmsManager mSmsManager = SmsManager.getDefault();
-        mSmsManager.sendTextMessage(smsNumber, null, smsText, sentIntent, deliveredIntent);
+    	// TODO Auto-generated method stub
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.activity_main);
+    	
+    	Button nextB = (Button)findViewById(R.id.next_button);
+    	nextB.setOnClickListener(new Button.OnClickListener() {
+    		public void onClick(View v) {
+    			Intent intent = new Intent(MainActivity.this, SendSMSActivity.class);
+    			startActivity(intent);
+    		}
+    	});
     }
 }
