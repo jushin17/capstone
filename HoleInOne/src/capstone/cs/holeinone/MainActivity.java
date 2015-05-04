feefe5c1deae85ec1ad8e960d8c552109a115112
HyunSongKim <gusthd92@gmail.com>
2015-05-04 22:06:29 +0900

    bluetooth

--- HoleInOne/src/capstone/cs/holeinone/MainActivity.java
+++ HoleInOne/src/capstone/cs/holeinone/MainActivity.java
@@ -1,18 +1,10 @@
 package capstone.cs.holeinone;
 
-import capstone.cs.holeinone.R;
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
@@ -26,6 +18,14 @@ public class MainActivity extends Activity {
     		public void onClick(View v) {
     			Intent intent = new Intent(MainActivity.this, SendSMSActivity.class);
     			startActivity(intent);
+    		}
+    	});
+     	    	
+    	Button blue = (Button)findViewById(R.id.blue_button);
+    	blue.setOnClickListener(new Button.OnClickListener() {
+    		public void onClick(View v) {
+    			Intent intent2 = new Intent(MainActivity.this, Bluetooth.class);
+    			startActivity(intent2);
     		}
     	});
     }
